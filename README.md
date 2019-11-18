09c0fbf4c5946bf4439268c06b1b25e523def082
Olivia OLeary <olearyo@sheridancollege.ca>
2019-11-18 11:20:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bountiful
+ A meal-kit delivery website that donates every meal you purchase to a foodbank near you
---
+++ home.php
@@ -0,0 +1,97 @@
+<?php
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" href="css/core.css" />
+    <link rel="stylesheet" href="https://use.typekit.net/xqv5gtt.css">
+
+
+
+</head>
+<body>
+
+  <header>
+    <nav>
+    <a id="logo" href="home.php">bountiful</a>
+
+        <p class="menu">
+            <a href="home.php">HOME</a>
+            <a href="about.php">ABOUT</a>
+            <a href="menu.php">MENU</a>
+            <a href="register.php">REGISTER</a>
+            <a href="login.php">LOGIN</a>
+        </p>
+    </nav>
+  </header>
+
+
+
+    <div class="hero-image">
+      <!-- <h1 class="hero-title">Food is best served together</h1> -->
+
+    </div>
+
+
+  <main>
+
+  <h1>How it works:</h1>
+       <div class="showcase">
+            <div class="one">
+                <img class="icons" src="img/choose.svg"/>
+            </div>
+            <div class="two">
+                <img class="icons" src="img/deliver.svg"/>
+            </div>
+            <div class="three">
+                <img class="icons" src="img/donate.svg"/>
+            </div>
+        </div>
+
+        <div class="showcase-title">
+            <div class="choose">
+                <h2>Choose your meal plan</h2>
+            </div>
+            <div class="deliver">
+                <h2>Select a foodbank near you</h2>
+            </div>
+            <div class="donate">
+                <h2>Donate! it's a win-win</h2>
+            </div>
+        </div>
+
+
+  <h1>Latest news</h1>
+       <div class="news">
+            <div class="news-box">
+                <p>Updated needs list for Ontario</p>
+            </div>
+            <div class="news-box">
+                <p>Menu of the week</p>
+            </div>
+            <div class="news-box">
+                <p>HungerCount 2019</p>
+            </div>
+        </div>
+
+
+
+
+
+
+
+        <div class="news">
+            <div class="four"><p>two</p></div>
+            <div class="five"><p>three</p></div>
+            <div class="six"><p>four</p></div>
+        </div>
+
+
+  </main>
+  <!-- <footer>
+    <p>subscribe!</p>
+  </footer> -->
+</body>
+</html>
