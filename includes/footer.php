b215e5e88355c089a6b00b26852c43419a9515d9
Olivia OLeary <olearyo@sheridancollege.ca>
2019-11-26 00:10:25 -0500

    Sticky newsletter

--- includes/footer.php
+++ includes/footer.php
@@ -6,11 +6,11 @@
 
 <a id="footer-logo" href="home.php">bountiful</a>
 
-<div class="newsletter">
-    <h2 class="white">Subscribe to stay in the loop!</h2>
+<div class="newsletter sticky">
     <form action="process-subscribe.php" method="POST">
+        <div class="subscribe-label"><h2 class="white">Subscribe to stay in the loop!</h2></div>
 
-        <input class="subscribe-field" type="email" name="email" placeholder="email" required />
+        <div class="subscribe-field"><input type="email" name="email" placeholder="email" required /></div>
         <input class="subscribe-button" type="submit" name="submit" value="SUBSCRIBE"/>
 
     </form>
